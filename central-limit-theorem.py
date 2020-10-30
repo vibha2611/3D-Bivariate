b3d22a06889dbdfbf455316ae391beb0f4226de5
Vibha Padmanabhan <57306517+vibhapadmanabhan@users.noreply.github.com>
2020-10-30 12:39:58 +0530

    Style fixes

--- central-limit-theorem.py
+++ central-limit-theorem.py
@@ -1,38 +1,38 @@
-#importing libraries
+# Importing libraries
 import statistics
 import matplotlib.pyplot as plt
 import pandas as pd
 from random import sample
 
-#extracting column of data
-population=pd.read_excel(filename)
-population=population["column_name"]
+# Extracting column of data
+population = pd.read_excel(filename)
+population = population["column_name"]
 
-#initialise lists for data and means
-column_name=[]
-sample_means=[]
+# Initialise lists for data and means
+column_name = []
+sample_means = []
 
-#set sampling parameters
-number_of_samples=int(input("Enter number of samples: "))
-sample_size=int(input("Enter sample size: "))
+# Set sampling parameters
+number_of_samples = int(input("Enter number of samples: "))
+sample_size = int(input("Enter sample size: "))
 
-#converting the population into list form
+# Converting the population into list form
 for i in range(len(population)):
     column_name.append(population[i])
     
-#sampling
-for i in range(number_of_samples):
-    x=statistics.mean(sample(column_name, sample_size))
+# Sampling
+for _ in range(number_of_samples):
+    x = statistics.mean(sample(column_name, sample_size))
     sample_means.append(x)
 
-#calculating population and sample descriptives
+# Calculating population and sample descriptives
 print(statistics.mean(period))
 print(statistics.variance(period))
 print(statistics.mean(sample_means))
 print(statistics.variance(sample_means))
 
-#visualising the data
-plt.hist(sample_means, bins=sqrt(sample_size))
+# Visualising the data
+plt.hist(sample_means, bins = sqrt(sample_size))
 plt.xlabel("Sample means")
 plt.ylabel("Frequency")
 plt.show()
